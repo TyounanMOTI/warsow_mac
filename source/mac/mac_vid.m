8cb5af42cb054a778b9749cb7c1d5f8233abf973
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2012-07-04 20:34:48 +0900

    implement VID_GetScreenSize

--- source/mac/mac_vid.m
+++ source/mac/mac_vid.m
@@ -1,24 +1,25 @@
 /*
  Copyright (C) 1997-2001 Id Software, Inc.
- 
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
  of the License, or (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- 
+
  See the GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- 
+
  */
 
 #include "../client/client.h"
+#import <AppKit/NSScreen.h>
 
 static int VID_WndProc( void *wnd, int ev, int p1, int p2 )
 {
@@ -31,7 +32,7 @@ static int VID_WndProc( void *wnd, int ev, int p1, int p2 )
 int VID_Sys_Init( int x, int y, int width, int height, qboolean fullScreen, qboolean wideScreen, qboolean verbose )
 {
 	extern cvar_t *vid_parentwid;
-  
+
 	return R_Init( NULL, NULL, (void *)(strtol( vid_parentwid->string, NULL, 0 )), 
                 x, y, width, height, fullScreen, wideScreen, verbose );
 }
@@ -86,5 +87,14 @@ void VID_FlashWindow( int count )
  */
 qboolean VID_GetScreenSize( int *width, int *height )
 {
+  NSScreen* screen = [NSScreen mainScreen];
+  if (screen)
+  {
+    NSRect rect = [screen frame];
+    *width = floor(rect.size.width);
+    *height = floor(rect.size.height);
+    return qtrue;
+  }
+
 	return qfalse;
 }
