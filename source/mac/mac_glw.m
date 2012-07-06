ce52d8a83a99bcd3d3195ba047db061943e45cdc
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2012-07-07 03:28:44 +0900

    implement Sys_OpenURLInBrowser

--- source/mac/mac_glw.m
+++ source/mac/mac_glw.m
@@ -256,4 +256,9 @@ void Sys_FreeClipboardData( char *data )
 	Q_free( data );
 }
 
-void	Sys_OpenURLInBrowser( const char *url ) {}
+void	Sys_OpenURLInBrowser( const char *url )
+{
+  NSString *string_url = [NSString stringWithUTF8String:url];
+  NSURL *ns_url = [NSURL URLWithString:string_url];
+  [[NSWorkspace sharedWorkspace] openURL:ns_url];
+}
